340bcd54ff702021091defc14de3e367730d238d
ignbagusindra <ignbagusindra.82@gmail.com>
2023-08-08 15:13:44 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome! Ticket is $5. Please insert cash.");
+
+int cash = Convert.ToInt32(Console.ReadLine());
+
+if (cash < 5)
+{
+    Console.WriteLine("Not enough money dudes!");
+}
+else if (cash == 5)
+{
+    Console.WriteLine("Here's your ticket!");
+}
+else
+{
+    int change = cash - 5;
+    Console.WriteLine("Here's your ticket, and $" + change + " change");
+}
+
+
+
+//Brackeys
+Console.ReadKey();
